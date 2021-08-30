3b59734b4f57e70d8abe6430126b08f5525850e0
elifelse <elifgsavukcam@gmail.com>
2021-08-30 11:57:34 +0300

    V1 start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SessionExp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Kisi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SessionExp.Models
+{
+    public class Kisi
+    {
+        [Display(Name = "Adınız")]
+        [Required(ErrorMessage = "Ad alanı zorunludur.")]
+        [MaxLength(20, ErrorMessage = "{0} en fazla {1} karakter içerebilir.")]
+        public string Ad { get; set; }
+    }
+}
